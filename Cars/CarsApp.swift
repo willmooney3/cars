350d43a4df7fb93cceeb53b3aeb10a88c4934a32
Will Mooney <will.mooney.3@gmail.com>
2023-10-09 22:17:04 -0400

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by William Mooney on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
